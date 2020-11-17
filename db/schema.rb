12997a3386ef3ba5f6f79c0be7187f6bacd3b065
suzu12 <1225suzuki8@gmail.com>
2020-11-17 21:29:56 +0900

    Teamsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_17_064218) do
+ActiveRecord::Schema.define(version: 2020_11_17_120335) do
+
+  create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "category_id", null: false
+    t.string "team_name", null: false
+    t.text "introduction", null: false
+    t.integer "period_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
