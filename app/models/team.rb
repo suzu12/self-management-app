76f68c44799df5e587e914d5ad21f6d7d0ec0fce
suzu12 <1225suzuki8@gmail.com>
2020-11-18 18:57:05 +0900

    ActiveHashとリレーションとバリデーション

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,15 @@
 class Team < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :period
+
+  with_options presence: true do
+    validates :category_id
+    validates :team_name, length: { maximum: 15 }
+    validates :period_id
+    validates :introduction, length: { maximum: 400 }
+  end
+
+  validates :category_id, numericality: { other_than: 1, message: 'を選択してください' }
+  validates :period_id, numericality: { other_than: 1, message: 'を選択してください' }
 end
