feae443963e4753c174f6c21cd2a17ed8df5fa68
suzu12 <1225suzuki8@gmail.com>
2020-11-19 18:07:36 +0900

    チームを作成できるようにした

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,27 @@
 class TeamsController < ApplicationController
+  # before_action :authenticate_user! only: [:index]
+
   def index
-    
+    @teams = Team.all.order('created_at DESC')
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def create
+    @team = Team.new(team_params)
+    if @team.save
+      redirect_to root_path, notice: '保存できました'
+    else
+      flash.now[:error] = '保存に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def team_params
+    params.require(:team).permit(:category_id, :team_name, :introduction, :period_id, :image)
   end
 end
