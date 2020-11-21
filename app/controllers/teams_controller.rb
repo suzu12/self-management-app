afa9d83c7bddb2836199c7249772de8fcb5a5b8f
suzu12 <1225suzuki8@gmail.com>
2020-11-21 17:46:48 +0900

    userとteamはteam_usersテーブルに保存される

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,5 +1,5 @@
 class TeamsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_team, only: [:show, :edit, :update, :destroy]
 
   def index
@@ -8,18 +8,18 @@ class TeamsController < ApplicationController
 
   def new
     @team = Team.new
+    @team.users << current_user
   end
 
   def create
-    @team = Team.new(team_params)
-    if @team.save
+    if current_user.teams.create(team_params)
       redirect_to root_path, notice: '保存できました！'
     else
       flash.now[:error] = '保存に失敗しました'
       render :new
     end
   end
-  
+
   def update
     if @team.update(team_params)
       redirect_to team_path(@team), notice: '編集できました！'
@@ -28,17 +28,18 @@ class TeamsController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
-    @team.destroy!
-    redirect_to root_path, notice: '削除することができました！'
+    if user_signed_in? && current_user.has_made?(@team)
+      @team.destroy!
+      redirect_to root_path, notice: '削除することができました！'
+    end
   end
-  
 
   private
 
   def team_params
-    params.require(:team).permit(:category_id, :team_name, :introduction, :period_id, :image)
+    params.require(:team).permit(:category_id, :team_name, :introduction, :period_id, :image, user_ids: [])
   end
 
   def set_team
