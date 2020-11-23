c607c19eb237825f43f401f7c6f909600c9830b7
suzu12 <1225suzuki8@gmail.com>
2020-11-23 11:23:35 +0900

    チャットモデルを作成

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,4 +1,11 @@
 class Chat < ApplicationRecord
-  belongs_to :user
-  belongs_to :chat
+  belongs_to :user, optional: true
+  belongs_to :chat, optional: true
+
+  has_one_attached :photo
+
+  validates :content, presence: true, unless: :was_attached?
+  def was_attached?
+    photo.attached?
+  end
 end
