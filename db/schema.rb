55774438a65f71c8e8f2365cbe1e7cc6fbe48210
suzu12 <1225suzuki8@gmail.com>
2020-11-25 16:56:55 +0900

    commentsテーブルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_115658) do
+ActiveRecord::Schema.define(version: 2020_11_25_075138) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 2020_11_23_115658) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["team_id"], name: "index_chats_on_team_id"
     t.index ["user_id"], name: "index_chats_on_user_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "team_id"
+    t.text "content", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_comments_on_team_id"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -96,6 +104,7 @@ ActiveRecord::Schema.define(version: 2020_11_23_115658) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "chats", "teams"
   add_foreign_key "chats", "users"
+  add_foreign_key "comments", "teams"
   add_foreign_key "likes", "teams"
   add_foreign_key "likes", "users"
   add_foreign_key "profiles", "users"
