e13299f3edd373cd552e286f1f70e49796d49894
suzu12 <1225suzuki8@gmail.com>
2020-11-26 19:17:08 +0900

    CommentsControllerでアクション内の処理を定義

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  before_action :set_comment
+
+  def index
+    comments = @team.comments
+    render json: comments
+  end
+
+  def create
+    @comment = @team.comments.new(comment_params)
+    @comment.save!
+
+    render json: @comment
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+  def set_comment
+    @team = Team.find(params[:team_id])
+  end
+end
