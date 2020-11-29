41d7551cc70f5d8b7976e4100bd4fce4dc42ebb8
suzu12 <1225suzuki8@gmail.com>
2020-11-29 20:48:43 +0900

    見やすいように空行を開けた

--- app/models/team.rb
+++ app/models/team.rb
@@ -4,8 +4,10 @@ class Team < ApplicationRecord
   belongs_to :period
 
   has_one_attached :image
+
   has_many :team_users
   has_many :users, through: :team_users, dependent: :destroy
+
   has_many :chats, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :comments, dependent: :destroy
