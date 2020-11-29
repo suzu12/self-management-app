5c660e69bfb16b4366a9e6bbadb8f4e8db8bc8f2
suzu12 <1225suzuki8@gmail.com>
2020-11-29 14:53:10 +0900

    followとunfollowができるようにurlを生成した

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'teams#index'
+
   resources :teams do
     resources :chats, only: %i(index create)
     resources :team_users, only: :create
     resources :comments, only: %i(index create)
     resource :like, only: %i(show create destroy)
   end
-  resources :profiles, only: %i(show edit update)
+
+  resources :accounts, only: :show do
+    resources :follows, only: :create
+    resources :unfollows, only: :create
+  end
+
+  resource :profile, only: %i(show edit update)
 end
