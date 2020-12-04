a359f7db28fb16659e72e5088368fe236d6bbdc1
suzu12 <1225suzuki8@gmail.com>
2020-12-04 16:17:16 +0900

    タグを更新するときのメソッドをモデルに定義

--- app/models/team.rb
+++ app/models/team.rb
@@ -14,4 +14,19 @@ class Team < ApplicationRecord
 
   has_many :team_tag_relations
   has_many :tags, through: :team_tag_relations
+
+  def update_tags(update_team_tags)
+    current_tags = tags.pluck(:name) unless tags.nil?
+    old_tags = current_tags - update_team_tags
+    new_tags = update_team_tags - current_tags
+
+    old_tags.each do |old_name|
+      tags.delete Tag.find_by(name: old_name)
+    end
+
+    new_tags.each do |new_name|
+      teams_tag = Tag.find_or_create_by(name: new_name)
+      tags << teams_tag
+    end
+  end
 end
