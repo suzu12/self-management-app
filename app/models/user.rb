7a8d4638f48a8cabd700be985110f3783d1d83f0
suzu12 <1225suzuki8@gmail.com>
2020-12-10 18:36:25 +0900

    SNS認証のときは自動でパスワードが生成される

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'は英字と数字の両方を含めて設定してください'
+
   delegate :birthday, :age, :gender, :bio, to: :profile, allow_nil: true
 
   def self.from_omniauth(auth)
@@ -34,7 +37,7 @@ class User < ApplicationRecord
 
     if user.persisted?
       sns.user = user
-      sns.save
+      sns.save(validate: false)
     end
     { user: user, sns: sns }
   end
