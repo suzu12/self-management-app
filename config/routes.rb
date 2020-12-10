7e94207a73f2aea736e5d49105fd4d6dc8f98ada
suzu12 <1225suzuki8@gmail.com>
2020-12-10 15:12:32 +0900

    SNS認証用のテーブルを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'teams#index'
   resources :filtering_searchs, only: %i(index)
