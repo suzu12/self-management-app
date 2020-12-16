7c250f41b664e2bc01675348e23bf12aed67b7ad
suzu12 <1225suzuki8@gmail.com>
2020-12-16 16:56:40 +0900

    リレーションの修正

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,6 +2,8 @@ class Chat < ApplicationRecord
   belongs_to :user, optional: true
   belongs_to :chat, optional: true
 
+  has_one :nickname
+  has_one :nickname, through: :user
   has_one_attached :photo
 
   validates :content, presence: true, unless: :was_attached?
